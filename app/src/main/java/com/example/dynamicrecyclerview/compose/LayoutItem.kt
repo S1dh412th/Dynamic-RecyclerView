00489eb34c358a6931ebeda4f212161762648f33
S1dh412th <55277309+S1dh412th@users.noreply.github.com>
2022-08-16 11:37:46 +0530

    Implemented chat layout (#1)

--- app/src/main/java/com/example/dynamicrecyclerview/compose/LayoutItem.kt
+++ app/src/main/java/com/example/dynamicrecyclerview/compose/LayoutItem.kt
@@ -1,5 +1,6 @@
 package com.example.dynamicrecyclerview.compose
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -9,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -20,37 +22,44 @@ import com.example.dynamicrecyclerview.data.model.DynamicLayout
 
 @Composable
 fun LayoutItem(layout: DynamicLayout, buttonOnClick: (String)->(Unit)) {
-    Card(
-        modifier = Modifier
-            .padding(10.dp)
-            .fillMaxWidth(),
-        elevation = 10.dp,
-        shape = RoundedCornerShape(corner = CornerSize(10.dp))
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = if(layout.isSent) Alignment.End else Alignment.Start
     ) {
-        Column(
-            modifier = Modifier.padding(5.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+        Card(
+            modifier = Modifier
+                .padding(10.dp),
+            elevation = 10.dp,
+            shape = RoundedCornerShape(corner = CornerSize(10.dp))
         ) {
-            if (layout.image.isImage) {
-                AsyncImage(
-                    model = ImageRequest.Builder(LocalContext.current)
-                        .data(layout.image.imgUrl)
-                        .build(),
-                    placeholder = painterResource(R.drawable.ic_launcher_foreground),
-                    contentDescription = "Image",
-                )
-            }
-            Spacer(modifier = Modifier.height(8.dp))
-            if (layout.button.isButton) {
-                Button(onClick = {
-                    buttonOnClick(layout.button.value)
-                }) {
-                    Text(text = layout.button.value)
+            Column(
+                modifier = Modifier
+                    .background(color = if(layout.isSent) Color.Cyan else Color.LightGray)
+                    .padding(10.dp),
+                horizontalAlignment = if(layout.isSent) Alignment.End else Alignment.Start,
+
+            ) {
+                if (layout.image.isImage) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(layout.image.imgUrl)
+                            .build(),
+                        placeholder = painterResource(R.drawable.ic_launcher_foreground),
+                        contentDescription = "Image",
+                    )
                 }
-            }
-            Spacer(modifier = Modifier.height(8.dp))
-            if (layout.text.isText) {
-                Text(text = layout.text.value)
+                Spacer(modifier = Modifier.height(8.dp))
+                if (layout.button.isButton) {
+                    Button(onClick = {
+                        buttonOnClick(layout.button.value)
+                    }) {
+                        Text(text = layout.button.value)
+                    }
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                if (layout.text.isText) {
+                    Text(text = layout.text.value)
+                }
             }
         }
     }
